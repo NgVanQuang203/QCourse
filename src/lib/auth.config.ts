6feb1abcdb91be736773be26228b5142a5353822
Quang Nguyen <vanquang20011@gmail.com>
2026-04-08 00:18:13 +0700

    fix: explicitly map auth secret to support both AUTH_SECRET and NEXTAUTH_SECRET

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -22,7 +22,9 @@ declare module 'next-auth' {
 
 
 export const authConfig = {
+  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
   // ── Providers ──────────────────────────────────────────────────────────────
+
   providers: [
     // Google OAuth
     Google({
